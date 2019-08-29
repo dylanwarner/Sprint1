3bce78aa9291980823462745b8eda1ebfccd6468
Dylan Warner <dcw8084@g.rit.edu>
2019-08-29 10:10:33 -0400

    Added file upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function uploadImage() {
+    var image = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+        image.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        image.src = "";
+    }
+
+    uploadImage();
+}
