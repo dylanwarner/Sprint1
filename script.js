b383de418cdf45a8e58c218fb0451c16441f0f25
Cimo02 <tjc3255@g.rit.edu>
2019-09-02 17:13:57 -0400

    fixed draggability, and added dblclick to scale, filters, remaining stickers

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function uploadImage() {
         image.src = "";
     }
 }
+
+function changeFilter (filter) {
+    var img = document.querySelector('img');
+    switch(filter){
+        /* Filters for Images */
+        case 'normal': 
+            img.style.webkitFilter = "none";
+            img.filter = "none";
+            break;
+        case 'grayscale': 
+            img.style.webkitFilter = "grayscale(100%)";
+            img.filter = "grayscale(100%)";
+            break;
+        case 'hue90': 
+            img.style.webkitFilter = "hue-rotate(90deg)";
+            img.filter = "hue-rotate(90deg)";
+            break;
+        case 'hue180': 
+            img.style.webkitFilter = "hue-rotate(180deg)";
+            img.filter = "hue-rotate(180deg)";
+            break;
+        case 'hue270': 
+            img.style.webkitFilter = "hue-rotate(270deg)";
+            img.filter = "hue-rotate(270deg)";
+            break;
+        case 'inverted': 
+            img.style.webkitFilter = "invert(100%)";
+            img.filter = "invert(100%)";
+            break;
+        case 'saturated': 
+            img.style.webkitFilter = "saturate(200%)";
+            img.filter = "saturate(200%)";
+            break;
+        case 'sepia': 
+            img.style.webkitFilter = "sepia(100%)";
+            img.filter = "sepia(100%)";
+            break;
+    }
+}
--- stickers.js
+++ stickers.js
@@ -1,45 +1,69 @@
 // make elements draggable       
 // search for all draggable items
-console.log('stickers loads');
-dragElement(document.getElementsByClassName("draggable"));
+var cursorX = 0;
+var cursorY = 0;
+var elmntX = 0;
+var elmntY = 0;
+var elmnt = null;
+var stickerClone = null;
 
-// iterate over all of the items that are .draggable
-function dragElement(elmnts){
-    console.log('function called');
-    Array.from(elmnts).forEach((elmnt) => {
-        console.log('loop started');
-        var elmntX = 0, elmntY = 0, cursorX = 0, cursorY = 0;
-        
-        elmnt.onmousedown = dragMouseDown;
-    
-        function dragMouseDown(e) {
-          e = e || window.event;
-          e.preventDefault();
-          // get the mouse cursor position at startup:
-          cursorX = e.clientX;
-          cursorY = e.clientY;
-          document.onmouseup = closeDragElement;
-          // call a function whenever the cursor moves:
-          document.onmousemove = elementDrag;
-        }
-        
-        function elementDrag(e) {
-          e = e || window.event;
-          e.preventDefault();
-          // calculate the new cursor position:
-          elmntX = cursorX - e.clientX;
-          elmntY = cursorY - e.clientY;
-          cursorX = e.clientX;
-          cursorY = e.clientY;
-          // set the element's new position:
-          elmnt.style.top = (elmnt.offsetTop - elmntY) + "px";
-          elmnt.style.left = (elmnt.offsetLeft - elmntX) + "px";
-        }
-        
-        function closeDragElement() {
-          // stop moving when mouse button is released:
-          document.onmouseup = null;
-          document.onmousemove = null;
-        }
-    });
+function dragMouseDown(e, element) {
+  elmnt = element;
+  e = e || window.event;
+  e.preventDefault();
+  // get the mouse cursor position at startup:
+  cursorX = e.clientX;
+  cursorY = e.clientY;
+  if (element.id == 'original'){
+    stickerClone = element.cloneNode(true);
+    element.id = '';
+  }
+  document.onmouseup = closeDragElement;
+  // call a function whenever the cursor moves:
+  document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // calculate the new cursor position:
+  elmntX = cursorX - e.clientX;
+  elmntY = cursorY - e.clientY;
+  cursorX = e.clientX;
+  cursorY = e.clientY;
+  // set the element's new position:
+  if (elmnt.style.transform == "scale(0.75)"){
+    elmnt.style.transform = "scale(0.77)";
+  }
+  else if (elmnt.style.transform == "scale(0.5)"){
+    elmnt.style.transform = "scale(0.52)";
+  }
+  else if (elmnt.style.transform == "scale(0.52)" || elmnt.style.transform == "scale(0.77)"){}
+  else {
+    elmnt.style.transform = "scale(0.52)";
+  }
+  elmnt.style.top = (elmnt.offsetTop - elmntY) + "px";
+  elmnt.style.left = (elmnt.offsetLeft - elmntX) + "px";
+}
+
+function closeDragElement() {
+  // stop moving when mouse button is released:
+  if (elmnt.style.transform == "scale(0.77)"){
+    elmnt.style.transform = "scale(0.75)";
+  }
+  else {
+    elmnt.style.transform = "scale(0.5)"
+  }
+  document.onmouseup = null;
+  document.onmousemove = null;
+  document.getElementById('image-scroller').appendChild(stickerClone);
+}
+
+function scaleSticker (sticker) {
+  if (sticker.style.transform == "scale(0.5)"){
+    sticker.style.transform = "scale(0.75)";
+  }
+  else{
+    sticker.style.transform = "scale(0.5)"
+  }
 }
